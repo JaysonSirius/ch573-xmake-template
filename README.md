fdbe414f93609d5f9fae257bc2d9f53fedadfbf1
s1rius <lkzukuuxiao@gmail.com>
2022-09-09 20:29:00 +0800

    feat: 增加CH573基础SDK的xmake工程编译，目前仅测试了ADC例程

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ch573-xmake-template
+使用xmake编译CH573项目
 Build CH573(a 32-bit RISC-V Bluetooth Low energy MCU) by xmake.
+
+----
+## 1. TODO list
+:white_check_mark: 1) xmake工程创建
+:white_check_mark: 2) CH573基础SDK编译（参考ADC例程）
+:white_large_square: 3) 除BLE和USB外的例程编译
+:white_large_square: 4) USB的SDK添加及例程编译
+:white_large_square: 5) BLE的SDK添加及例程编译
+
+----
+## 2. 如何使用
+
+### 2.1 测试环境
+硬件：MacBook Air (M1, 2020)
+系统：macOS Monterey 12.5.1
+
+### 2.2 编译方法
+1) 下载编译工具链并将其解压至任意目录
+```
+curl -O http://file.mounriver.com/tools/MRS_Toolchain_MAC_V150.zip
+```
+
+2) 设置环境变量`WCH_RISCV_SDK`或在*xmake.lua*中指定编译工具链路径，见
+```
+local sdk_dir = os.getenv("WCH_RISCV_SDK")
+    or "opt/wchtools/xpack-riscv-none-embed-gcc-8.2.0"
+```
+
+3) 在`./src`中添加自己的项目代码后编译即可
+```
+xmake
+```
+若想在编译过程中查看详情则可使用
+```
+xmake -v
+```
+
+4) 在`./build/cross/riscv`中即可找到编译生成的文件
---
+++ xmake.lua
@@ -0,0 +1,196 @@
+-- 设置工程名
+set_project("ch573-xmake")
+-- 设置工程版本
+set_version("0.1.0", {build = "%Y%m%d%H%M"})
+
+add_rules("mode.debug", "mode.release")
+
+-- 定义交叉编译工具链
+toolchain("wch-riscv-gcc")
+    set_kind("standalone")
+    local sdk_dir = os.getenv("WCH_RISCV_SDK")
+        or "opt/wchtools/xpack-riscv-none-embed-gcc-8.2.0"
+    set_sdkdir(sdk_dir)
+toolchain_end()
+
+-- 禁用flags的自动检测和忽略机制
+set_policy("check.auto_ignore_flags", false)
+
+target("ch573")
+    -- 编译为二进制程序
+    set_kind("binary")
+    -- 设置编译平台
+    set_plat("cross")
+    -- 设置编译架构
+    set_arch("riscv")
+    -- 设置编译工具链
+    set_toolchains("wch-riscv-gcc")
+    -- 设置生成文件名
+    set_filename("ch573.elf")
+    -- 设置优化级别，这里是-Os最小化代码优化
+    set_optimize("smallest")
+    -- 设置语言标准
+    set_languages("gnu99", "cxx11")
+    -- 添加宏定义
+    add_defines("DEBUG=1")
+    -- 添加C/C++编译选项
+    add_cxflags(
+        "-march=rv32imac",
+        "-mabi=ilp32",
+        "-mcmodel=medany",
+        "-msmall-data-limit=8",
+        "-mno-save-restore",
+        "-fmessage-length=0",
+        "-fsigned-char",
+        "-ffunction-sections",
+        "-fdata-sections",
+        "-fno-common",
+        "-Wunused",
+        "-g"
+    )
+    -- 添加汇编编译选项
+    add_asflags(
+        "-march=rv32imac",
+        "-mabi=ilp32",
+        "-mcmodel=medany",
+        "-msmall-data-limit=8",
+        "-mno-save-restore",
+        "-fmessage-length=0",
+        "-fsigned-char",
+        "-ffunction-sections",
+        "-fdata-sections",
+        "-fno-common",
+        "-Wunused",
+        "-g",
+        "-x assembler"
+    )
+    -- 添加静态链接选项
+    add_ldflags(
+        "-nostartfiles",
+        "-Xlinker --gc-sections",
+        "-Xlinker --print-memory-usage",
+        "--specs=nano.specs",
+        "-u _printf_float",
+        "--specs=nosys.specs"
+    )
+
+    -- 添加SDK相关文件
+        local sdk_path = "$(projectdir)/sdk"
+        -- 添加链接库搜索目录
+        add_linkdirs(sdk_path.."/StdPeriphDriver")
+        -- 添加链接库
+        add_links("ISP573")
+        -- 添加启动文件
+        add_files(sdk_path.."/Startup/startup_CH573.S")
+        -- 添加链接脚本
+        add_files(sdk_path.."/Ld/Link.ld")
+        -- 添加源文件
+        add_files(sdk_path.."/**.c")
+        -- 添加头文件搜索目录
+        add_includedirs(sdk_path.."/RVMSIS")
+        add_includedirs(sdk_path.."/StdPeriphDriver/inc")
+
+    -- 添加项目相关文件
+        local src_path = "$(projectdir)/src"
+        -- 添加源文件
+        add_files(src_path.."/**.c")
+        -- 添加头文件搜索目录
+        -- add_includedirs(os.dirs(src_path.."/**"))
+
+    -- 加载时操作
+    on_load(function (target)
+        local map_file = string.format("%s/%s.map",
+            target:targetdir(), target:name())
+        target:add("ldflags", "-Wl,-Map,"..map_file)
+    end)
+
+    -- 构建后操作
+    after_build(function (target)
+        cprint(" ");
+        cprint("${green}Compile finished!!! ${heavy_check_mark}")
+        cprint("generating hex files...")
+        os.exec("riscv-none-embed-objcopy -O ihex %s/%s %s/%s.hex",
+            target:targetdir(), target:filename(), target:targetdir(), target:name())
+        cprint("${green}Generate hex files ok!!! ${heavy_check_mark}")
+
+        cprint(" ");
+        os.exec("riscv-none-embed-size --format=berkeley %s/%s",
+            target:targetdir(), target:filename())
+        cprint(" ");
+    end)
+
+    -- 清理后操作
+    after_clean(function (target)
+        os.rm(target:targetdir().."/*")
+    end)
+target_end()
+
+--
+-- If you want to known more usage about xmake, please see https://xmake.io
+--
+-- ## FAQ
+--
+-- You can enter the project directory firstly before building project.
+--
+--   $ cd projectdir
+--
+-- 1. How to build project?
+--
+--   $ xmake
+--
+-- 2. How to configure project?
+--
+--   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
+--
+-- 3. Where is the build output directory?
+--
+--   The default output directory is `./build` and you can configure the output directory.
+--
+--   $ xmake f -o outputdir
+--   $ xmake
+--
+-- 4. How to run and debug target after building project?
+--
+--   $ xmake run [targetname]
+--   $ xmake run -d [targetname]
+--
+-- 5. How to install target to the system directory or other output directory?
+--
+--   $ xmake install
+--   $ xmake install -o installdir
+--
+-- 6. Add some frequently-used compilation flags in xmake.lua
+--
+-- @code
+--    -- add debug and release modes
+--    add_rules("mode.debug", "mode.release")
+--
+--    -- add macro defination
+--    add_defines("NDEBUG", "_GNU_SOURCE=1")
+--
+--    -- set warning all as error
+--    set_warnings("all", "error")
+--
+--    -- set language: c99, c++11
+--    set_languages("c99", "c++11")
+--
+--    -- set optimization: none, faster, fastest, smallest
+--    set_optimize("fastest")
+--
+--    -- add include search directories
+--    add_includedirs("/usr/include", "/usr/local/include")
+--
+--    -- add link libraries and search directories
+--    add_links("tbox")
+--    add_linkdirs("/usr/local/lib", "/usr/lib")
+--
+--    -- add system link libraries
+--    add_syslinks("z", "pthread")
+--
+--    -- add compilation and link flags
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
+--
+-- @endcode
+--
+
